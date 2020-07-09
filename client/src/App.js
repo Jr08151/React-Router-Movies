9f2700539e48b6eb857d8fc15d2c798441406c70
Jr08151 <Jr08151@users.noreply.github.com>
2020-07-09 19:39:30 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import {Route} from 'react-router-dom';
+import Movie from './Movies/Movie.js';
+import MovieList from './Movies/MovieList.js';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+  const [savedList, setSavedList] = useState( [] );
+  const [savedIds, setSavedIds] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+    if (!savedIds.includes(movie.id)) {
+      setSavedIds([...savedIds, movie.id]);
+      setSavedList( [...savedList, movie] );
+    }
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" render={props=><Movie {...props} addToSavedList={addToSavedList} />}/>
     </div>
   );
 };
